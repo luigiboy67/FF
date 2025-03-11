b91a4bfe9a22e342ee52eec9b2b8afc0a9dc963a
luigiboy67 <shamgod98@gmail.com>
2025-03-10 20:05:36 -0400

    upload ff2 data

---
+++ R/final_fantasy_II_bestiary_data.R
@@ -0,0 +1,20 @@
+#' Final Fantasy Two Bestiary data
+#'
+#' Data on all monsters in Final Fantasy 2
+#'
+#' @format `final_fantasy_II_bestiary_data`
+#' A data frame with 128 rows and 10 columns
+#' \describe{
+#'    \item{Name}{Name}
+#'    \item{Rank}{Rank}
+#'    \item{HP}{Hit Points}
+#'    \item{MP}{Magic Points}
+#'    \item{Attack}{Attacks}
+#'    \item{Defense}{Defense}
+#'    \item{Magic Defense}{Magic Defense}
+#'    \item{Accuracy}{Accuracy}
+#'    \item{Evasion}{Evasion}
+#'    \item{Gil}{Gil}
+#' }
+#' @source <https://finalfantasy.fandom.com/wiki/Bestiary_(Final_Fantasy_II)>
+"final_fantasy_II_bestiary_data"
