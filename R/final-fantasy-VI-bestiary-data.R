d5d36b4161e2b6a6131e2ee94442c05b2a805cc6
luigiboy67 <shamgod98@gmail.com>
2025-03-24 23:39:21 -0400

    add data for ff6 and restructure all the R files in the R folder

---
+++ R/final-fantasy-VI-bestiary-data.R
@@ -0,0 +1,24 @@
+#' Final Fantasy Six Bestiary data
+#'
+#' Data on all monsters in Final Fantasy 6
+#'
+#' @format `final_fantasy_VI_bestiary_data`
+#' A data frame with 347 rows and 14 columns
+#' \describe{
+#'    \item{Name}{Name}
+#'    \item{LV}{Level}
+#'    \item{HP}{Hit Points}
+#'    \item{MP}{Magic Points}
+#'    \item{Attack}{Attack}
+#'    \item{Magic}{Magic}
+#'    \item{Defense}{Defense}
+#'    \item{Magic Defense}{Magic Defense}
+#'    \item{Magic Evasion}{Magic Evasion}
+#'    \item{Agility}{Agility}
+#'    \item{Hit Rate}{Hit Rate}
+#'    \item{Evasion}{Evasion}
+#'    \item{EXP}{EXP}
+#'    \item{Gil}{Gil}
+#' }
+#' @source <https://finalfantasy.fandom.com/wiki/Bestiary_(Final_Fantasy_VI)#Bestiary_(pixel_remaster)>
+"final_fantasy_VI_bestiary_data"
