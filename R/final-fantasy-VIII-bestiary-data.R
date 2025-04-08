54bc44782fd153bdda8bc2949919bf47f30669c4
luigiboy67 <shamgod98@gmail.com>
2025-04-08 19:32:11 -0400

    update ff8 data and description for other ff data

---
+++ R/final-fantasy-VIII-bestiary-data.R
@@ -0,0 +1,20 @@
+#' Final Fantasy Eight Bestiary data
+#'
+#' Data on all monsters in Final Fantasy 8
+#'
+#' @format `final_fantasy_VIII_bestiary_data`
+#' A data frame with 128 rows and 10 columns
+#' \describe{
+#'    \item{Name}{Name}
+#'    \item{LV}{Level}
+#'    \item{HP}{Hit Points}
+#'    \item{STR}{Strength}
+#'    \item{MAG}{Magic}
+#'    \item{VIT}{Vitality}
+#'    \item{SPR}{Spirit}
+#'    \item{SPD}{Speed}
+#'    \item{EVA}{Evasion}
+#'    \item{EXP}{Experience Points}
+#' }
+#' @source <https://finalfantasy.fandom.com/wiki/Final_Fantasy_VIII_enemies>
+"final_fantasy_VIII_bestiary_data"
