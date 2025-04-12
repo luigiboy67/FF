918a4ca774733914e83e97c75f2c109be4b96daa
luigiboy67 <shamgod98@gmail.com>
2025-04-12 13:21:31 -0400

    add ff9 data to the package

---
+++ R/final-fantasy-IX-bestiary-data.R
@@ -0,0 +1,27 @@
+#' Final Fantasy Nine Bestiary data
+#'
+#' Data on all monsters in Final Fantasy 9
+#'
+#' @format `final_fantasy_IX_bestiary_data`
+#' A data frame with 197 rows and 16 columns
+#' \describe{
+#'    \item{Name}{Name}
+#'    \item{LV}{Level}
+#'    \item{HP}{Hit Points}
+#'    \item{MP}{Magic Points}
+#'    \item{Strength}{Strength}
+#'    \item{Defense}{Defense}
+#'    \item{Evade}{Evade}
+#'    \item{Magic}{Magic}
+#'    \item{Magic Def}{Magic Defense}
+#'    \item{Magic Eva}{Magic Evasion}
+#'    \item{Attack}{Attack}
+#'    \item{Spirit}{Spirit}
+#'    \item{Speed}{Speed}
+#'    \item{Exp}{Experience Point}
+#'    \item{Gil}{Gil}
+#'    \item{AP}{Ability Point}
+#'
+#' }
+#' @source <https://finalfantasy.fandom.com/wiki/Monster_(Final_Fantasy_IX)>
+"final_fantasy_IX_bestiary_data"
