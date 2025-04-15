3e3fb084c6eb595bb92616431eaedd2bc1ddd25b
luigiboy67 <shamgod98@gmail.com>
2025-04-15 19:03:36 -0400

    add ff10 data to package

---
+++ R/final-fantasy-X-bestiary-data.R
@@ -0,0 +1,23 @@
+#' Final Fantasy Ten Bestiary data
+#'
+#' Data on all monsters in Final Fantasy 10
+#'
+#' @format `final_fantasy_X_bestiary_data`
+#' A data frame with 246 rows and 13 columns
+#' \describe{
+#'    \item{Name}{Name}
+#'    \item{HP}{Hit Points}
+#'    \item{MP}{Magic Points}
+#'    \item{AP}{Ability Points}
+#'    \item{Gil}{Gil}
+#'    \item{Strength}{Strength}
+#'    \item{Magic}{Magic}
+#'    \item{Defense}{Defense}
+#'    \item{Magic Defense}{Magic Defense}
+#'    \item{Agility}{Agility}
+#'    \item{Accuracy}{Accuracy}
+#'    \item{Evasion}{Evasion}
+#'    \item{Luck}{Luck}
+#' }
+#' @source <https://finalfantasy.fandom.com/wiki/Sphere_of_Monster>
+"final_fantasy_X_bestiary_data"
